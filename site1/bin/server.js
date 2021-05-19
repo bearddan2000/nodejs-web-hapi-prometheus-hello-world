dcae4f3cd73615e3536134e42df9be99a20578c9
bearddan2000 <bearddan2000@yahoo.com>
2021-05-18 22:56:50 -0400

    initial commit

---
+++ site1/bin/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const client = require("prom-client");
+const metrics = require('./metrics');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 8000,
+        host: 'site1'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            metrics.visit();
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/metrics',
+        handler: (request, h) => {
+            return h
+                .response(client.register.metrics())
+                .type(client.register.contentType);
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
